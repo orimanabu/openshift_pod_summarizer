0e16a2a63381e6b086c8593c9706b9a3eb52d33f
Manabu Ori <manabu.ori@gmail.com>
2022-09-29 23:53:03 +0900

    update for offline mode

--- samples/get_json.sh
+++ samples/get_json.sh
@@ -6,10 +6,11 @@ if [ x"$?" != x"0" ]; then
 	exit 1
 fi
 
-oc get pod -A -o json > pods.json
-oc get node -A -o json > nodes.json
-oc get replicaset -A -o json > replicasets.json
-oc get deployment -A -o json > deployments.json
-oc get statefulset -A -o json > statefulsets.json
-oc get daemonset -A -o json > daemonsets.json
-oc get catalogsource -A -o json > catalogsources.json
+# oc get pod -A -o json > pods.json
+# oc get node -A -o json > nodes.json
+# oc get replicaset -A -o json > replicasets.json
+# oc get deployment -A -o json > deployments.json
+# oc get statefulset -A -o json > statefulsets.json
+# oc get daemonset -A -o json > daemonsets.json
+# oc get catalogsource -A -o json > catalogsources.json
+kubectl get -A pod,node,replicaset,statefulset,deployment,catalogsource -o json > all.json
